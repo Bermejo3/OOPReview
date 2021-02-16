ed3d5c6e603621a00706665871e2f7db30440527
Bermejo3 <alberto.bermejo.corredor@gmail.com>
2021-02-16 15:02:22 +0100

    Primera subida

---
+++ testMobile.ts
@@ -0,0 +1,27 @@
+import { Mobile } from "./claseMobile"
+
+let nokia3210: Mobile = new Mobile("Nokia3210", "3210", "Nokia", 0, "grey", false, 1, 85);
+let iphone3G: Mobile = new Mobile("iPhone3G", "3G", "Apple", 8, "white", false, 1, 200)
+let samsung10: Mobile = new Mobile("Samsung Galaxy 10", "Galaxy 10", "Samsung", 512, "blue", true, 3, 500)
+
+for (let i=0; i<Object.keys(nokia3210).length;i++){
+    console.log(`${Object.keys(nokia3210)[i]}: ${nokia3210[Object.keys(nokia3210)[i]]}`)
+}
+console.log("\n");
+
+for (let i=0; i<Object.keys(iphone3G).length;i++){
+    console.log(`${Object.keys(iphone3G)[i]}: ${iphone3G[Object.keys(nokia3210)[i]]}`)
+}
+console.log("\n");
+
+for (let i=0; i<Object.keys(samsung10).length;i++){
+    console.log(`${Object.keys(samsung10)[i]}: ${samsung10[Object.keys(nokia3210)[i]]}`)
+}
+console.log("\n");
+
+nokia3210.is5G = true
+nokia3210.cameraNumber = 4
+
+for (let i=0; i<Object.keys(nokia3210).length;i++){
+    console.log(`${Object.keys(nokia3210)[i]}: ${nokia3210[Object.keys(nokia3210)[i]]}`)
+}

69204797ac824f1bf03fe10398fe6c0eee835519
Bermejo3 <alberto.bermejo.corredor@gmail.com>
2021-02-16 15:30:25 +0100

    Subida con el ejercicio resuelto

--- claseMobile.ts
+++ claseMobile.ts
@@ -1,13 +1,13 @@
 export class Mobile {
     //Atributos
-    public name: string;
-    public model: string;
-    public trademark: string;
-    public sdSize: number;
-    public color: string;
-    public is5G: boolean;
-    public cameraNumber: number;
-    public price: number;
+    private name: string;
+    private model: string;
+    private trademark: string;
+    private sdSize: number;
+    private color: string;
+    private is5G: boolean;
+    private cameraNumber: number;
+    private price: number;
 
     //Constructor
     constructor (name: string, model: string, trademark: string, sdSize: number, color: string, is5G: boolean, cameraNumber: number, price: number){
@@ -21,5 +21,73 @@ export class Mobile {
         this.price = price
     }
 
+    // Metodos Getter y Setter
+    public getName(): string{
+        return this.name
+    }
+
+    public getModel(): string{
+        return this.model
+    }
+
+    public getTrademark(): string{
+        return this.trademark
+    }
+
+    public getSDSize(): number{
+        return this.sdSize
+    }
+
+    public getColor(): string{
+        return this.color
+    }
+
+    public getIs5g(): boolean{
+        return this.is5G
+    }
+
+    public getCameraNumber(): number{
+        return this.cameraNumber
+    }
+
+    public getPrice(): number{
+        return this.price
+    }
+
+    public setName(name:string){
+        this.name = name
+    }
+
+    public setModel(model: string){
+        this.model = model
+    }
     
+    public setTrademark(trademark: string){
+        this.trademark = trademark
+    }
+
+    public setSDSize(sdSize: number){
+        this.sdSize = sdSize
+    }
+
+    public setColor(color: string){
+        this.color = color
+    }
+
+    public setIs5G(is5G: boolean){
+        this.is5G = is5G
+    }
+
+    public setCameraNumber(cameraNumber: number){
+        this.cameraNumber = cameraNumber
+    }
+
+    public setPrice(price: number){
+        this.price = price
+    }
+
+    //Metodo publico
+    public characteristics():string{
+        return (`"The characteristics of the mobile ${this.name} are:"\n   · Name: ${this.name}\n   · Model: ${this.model}\n   · Trademark: ${this.trademark}\n   · SD Size (GB): ${this.sdSize}\n   · Color: ${this.color}\n   · Is 5g?: ${this.is5G}\n   · Number of Cameras: ${this.cameraNumber}\n`)
+    }
 }
--- testMobile.ts
+++ testMobile.ts
@@ -4,24 +4,44 @@ let nokia3210: Mobile = new Mobile("Nokia3210", "3210", "Nokia", 0, "grey", fals
 let iphone3G: Mobile = new Mobile("iPhone3G", "3G", "Apple", 8, "white", false, 1, 200)
 let samsung10: Mobile = new Mobile("Samsung Galaxy 10", "Galaxy 10", "Samsung", 512, "blue", true, 3, 500)
 
-for (let i=0; i<Object.keys(nokia3210).length;i++){
-    console.log(`${Object.keys(nokia3210)[i]}: ${nokia3210[Object.keys(nokia3210)[i]]}`)
-}
-console.log("\n");
+// for (let i=0; i<Object.keys(nokia3210).length;i++){
+//     console.log(`${Object.keys(nokia3210)[i]}: ${nokia3210[Object.keys(nokia3210)[i]]}`)
+// }
+// console.log("\n");
 
-for (let i=0; i<Object.keys(iphone3G).length;i++){
-    console.log(`${Object.keys(iphone3G)[i]}: ${iphone3G[Object.keys(nokia3210)[i]]}`)
-}
-console.log("\n");
+// for (let i=0; i<Object.keys(iphone3G).length;i++){
+//     console.log(`${Object.keys(iphone3G)[i]}: ${iphone3G[Object.keys(nokia3210)[i]]}`)
+// }
+// console.log("\n");
 
-for (let i=0; i<Object.keys(samsung10).length;i++){
-    console.log(`${Object.keys(samsung10)[i]}: ${samsung10[Object.keys(nokia3210)[i]]}`)
-}
-console.log("\n");
+// for (let i=0; i<Object.keys(samsung10).length;i++){
+//     console.log(`${Object.keys(samsung10)[i]}: ${samsung10[Object.keys(nokia3210)[i]]}`)
+// }
+// console.log("\n");
 
-nokia3210.is5G = true
-nokia3210.cameraNumber = 4
+// // Al cambiar de publico a privado ya no se puede solicitar
+// // nokia3210.is5G = true
+// // nokia3210.cameraNumber = 4
 
-for (let i=0; i<Object.keys(nokia3210).length;i++){
-    console.log(`${Object.keys(nokia3210)[i]}: ${nokia3210[Object.keys(nokia3210)[i]]}`)
+// for (let i=0; i<Object.keys(nokia3210).length;i++){
+//     console.log(`${Object.keys(nokia3210)[i]}: ${nokia3210[Object.keys(nokia3210)[i]]}`)
+// }
+
+console.log(nokia3210.characteristics());
+console.log(iphone3G.characteristics());
+console.log(samsung10.characteristics());
+
+nokia3210.setIs5G(true)
+nokia3210.setCameraNumber(4)
+
+console.log(nokia3210.characteristics());
+
+console.log("Array con los tres moviles juntos\n");
+
+let arrayMobiles: Mobile[] = [nokia3210, iphone3G, samsung10];
+
+for (let i=0; i<arrayMobiles.length; i++){
+    console.log(arrayMobiles[i].characteristics())
 }
+
+
